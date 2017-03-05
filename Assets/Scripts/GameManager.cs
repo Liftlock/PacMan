247c996fc3041107b2b7cc10b9a855f67ad32681
Robert French <rfrench@robertfrench.ca>
2017-03-05 12:42:58 -0500

    All the files!

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+﻿// using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    // TODO: 
+    // Add 3 lives ... have the ghosts cause death (placeholder ready)
+    // update the score 
+    // play some more music! 
+  
+}
